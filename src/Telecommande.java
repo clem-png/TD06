1269ebf0f1e2a1cbf3963271d00a3ada1a6fd7af
clem.png <clem.netange@gmail.com>
2023-04-05 17:45:55 +0200

    fin TD6

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -3,12 +3,19 @@ import java.util.ArrayList;
 public class Telecommande {
 
     private ArrayList<Lampe> lampes;
+    private ArrayList<Hifi> hifis;
     public Telecommande(){
         lampes = new ArrayList<Lampe>();
+        hifis = new ArrayList<Hifi>();
     }
+
 
     public void ajouterLampe(Lampe l){
         lampes.add(l);
+    }
+
+    public void ajouterHifi(Hifi hifi) {
+        hifis.add(hifi);
     }
 
     public void activerLampe(int indiceLampe){
@@ -19,9 +26,17 @@ public class Telecommande {
             System.out.println("indice trop grand");
         }
     }
+    public void activerHifi(int indiceHifi) {
+        hifis.get(indiceHifi).allumer();
+    }
 
     public void desactiverLampe(int indiceLampe){
         lampes.get(indiceLampe).eteindre();
+    }
+
+
+    public void desactiverHifi(int indiceHifi) {
+        hifis.get(indiceHifi).eteindre();
     }
 
     public void activerTout(){
@@ -31,9 +46,13 @@ public class Telecommande {
     }
 
     public String toString(){
-        String aff ="";
+        String aff ="lampe";
         for (int i = 0; i <lampes.size(); i++){
-            aff += lampes.get(i).toString();
+            aff += lampes.get(i).toString() + "\n";
+        }
+        aff += "hifi \n";
+        for (int i = 0; i <hifis.size(); i++){
+            aff += hifis.get(i).toString() + "\n";
         }
         return aff;
 
@@ -42,5 +61,7 @@ public class Telecommande {
     public ArrayList<Lampe> getLampe(){
         return lampes;
     }
+
+    public ArrayList<Hifi> getHifi() { return  hifis;}
 
 }
