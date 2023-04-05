38aeb2d94611958a186695821565d8dacace9162
clem.png <clem.netange@gmail.com>
2023-04-05 17:00:32 +0200

    modif gitignore + 2 méthodes Télécommande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 public class Telecommande {
 
     private ArrayList<Lampe> lampes;
     public Telecommande(){
-        throw new Error("code non écrit");
+        lampes = new ArrayList<Lampe>();
     }
 
     public void ajouterLampe(Lampe l){
-        throw new Error("code non écrit");
+        lampes.add(l);
     }
 
     public void activerLampe(int indiceLampe){
@@ -26,7 +26,5 @@ public class Telecommande {
     public String toString(){
         throw new Error("code non écrit");
     }
-=======
-package PACKAGE_NAME;public class Telecommande {
->>>>>>> 54c5dae9354c13fcc2c8faa744c5f8125c857404
+
 }
