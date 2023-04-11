e1105c3100640ca9fabb8a616ca365f265200d92
clem.png <clem.netange@gmail.com>
2023-04-11 11:45:26 +0200

    ajout de l'interface Appareil et modification de lampe/Hifi/telecommande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -2,66 +2,45 @@
 import java.util.ArrayList;
 public class Telecommande {
 
-    private ArrayList<Lampe> lampes;
-    private ArrayList<Hifi> hifis;
+    private ArrayList<Appareil> app;
     public Telecommande(){
-        lampes = new ArrayList<Lampe>();
-        hifis = new ArrayList<Hifi>();
+        app = new ArrayList<Appareil>();
     }
 
 
-    public void ajouterLampe(Lampe l){
-        lampes.add(l);
-    }
+    public void ajouterApp(Appareil l){app.add(l);}
 
-    public void ajouterHifi(Hifi hifi) {
-        hifis.add(hifi);
-    }
-
-    public void activerLampe(int indiceLampe){
+    public void activerApp(int indiceApp){
         try {
-            lampes.get(indiceLampe).allumer();
+            app.get(indiceApp).allumer();
         }
         catch (IndexOutOfBoundsException e ){
             System.out.println("indice trop grand");
         }
     }
-    public void activerHifi(int indiceHifi) {
-        hifis.get(indiceHifi).allumer();
-    }
 
-    public void desactiverLampe(int indiceLampe){
-        lampes.get(indiceLampe).eteindre();
-    }
-
-
-    public void desactiverHifi(int indiceHifi) {
-        hifis.get(indiceHifi).eteindre();
+    public void desactiverApp(int indiceApp){
+        app.get(indiceApp).eteindre();
     }
 
     public void activerTout(){
-        for (int i = 0; i <lampes.size(); i++){
-            lampes.get(i).allumer();
+        for (int i = 0; i <app.size(); i++){
+            app.get(i).allumer();
         }
     }
 
     public String toString(){
         String aff ="lampe";
-        for (int i = 0; i <lampes.size(); i++){
-            aff += lampes.get(i).toString() + "\n";
+        for (int i = 0; i <app.size(); i++){
+            aff += app.get(i).toString() + "\n";
         }
         aff += "hifi \n";
-        for (int i = 0; i <hifis.size(); i++){
-            aff += hifis.get(i).toString() + "\n";
+        for (int i = 0; i <app.size(); i++){
+            aff += app.get(i).toString() + "\n";
         }
         return aff;
 
     }
 
-    public ArrayList<Lampe> getLampe(){
-        return lampes;
-    }
-
-    public ArrayList<Hifi> getHifi() { return  hifis;}
 
 }
